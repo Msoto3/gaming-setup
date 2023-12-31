1c0b130e93778a2e532be3397662d935ea5c0140
Msoto3 <102495266+Msoto3@users.noreply.github.com>
2023-12-31 11:45:54 -0500

    Update Signup.js

--- frontend/src/Frontend/Signup.js
+++ frontend/src/Frontend/Signup.js
@@ -21,9 +21,9 @@ const Sign= () => {
   const createUser = async () =>{
     const data = {username:username}
     try{
-      const response = await axios.get("http://localhost:5000/routes/getUsers",{params:data})
+      const response = await axios.get("https://gaming-setup-api.vercel.app/routes/getUsers",{params:data})
       if(response.data.error) return false
-      await axios.post("http://localhost:5000/routes/createUser",{
+      await axios.post("https://gaming-setup-api.vercel.app/routes/createUser",{
         username,
         password,
         specs
